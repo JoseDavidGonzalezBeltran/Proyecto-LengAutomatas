0ac2bfc594ac58a6a326175e09bd80d1cab0e39c
JoseDavidGonzalezBeltran <158238784+JoseDavidGonzalezBeltran@users.noreply.github.com>
2025-11-03 20:47:58 +0000

    a

---
+++ CodigosPrueba.java
@@ -0,0 +1,26 @@
+public class CodigosPrueba {
+    
+//codigo con errores
+
+    /* "int x; float y; \n" +
+            "@\n" + // 1. Error Léxico
+            "if 5 == 5 \n" + // 2. Error Sintáctico (falta then)
+            "    output x; \n" +
+            "else \n" +
+            "    output y; \n" +
+            "while z == 10 \n" + // 3. Error Sintáctico (falta do)
+            "    input x;\n" +
+            "float mi_variable\n" + // 4. Error Sintáctico (falta ;)
+            "{\n" +
+            "    input x;\n" +
+            "    output y;\n"; // 5. Error Sintáctico (falta })*/
+
+//Codigo correcto
+           /*  "int x; float y; \n" +
+            "if 10 == 10 then \n" +
+            "  output x; \n" +
+            "else \n" +
+            "  { \n" +
+            "    while y do input y; \n" +
+            "  }";*/
+}
